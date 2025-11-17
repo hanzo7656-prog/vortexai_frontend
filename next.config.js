4c717cb56a1446e4ccba6424833b30b31270a95a
hanzo7656-prog <hanzo7656@gmail.com>
2025-11-17 15:09:57 +0330

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://ai-test-3gix.onrender.com',
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+      },
+    ]
+  }
+}
+
+module.exports = nextConfig
